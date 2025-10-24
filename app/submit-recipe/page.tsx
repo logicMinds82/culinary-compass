93f341cb7523c4008d11b02a97eb53c452e91e27
logicMinds82 <logic.minds82@gmail.com>
2025-10-25 01:08:05 +0300

    wrap SubmitRecipeClientWrapper comp in Suspense

--- app/submit-recipe/page.tsx
+++ app/submit-recipe/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from "react";
 import SubmitRecipeClientWrapper from "./SubmitRecipeClientWrapper";
+import RecipesSkeleton from "../components/RecipesSkeleton";
 
 export default function SubmitRecipePage() {
   return (
     <main>
-      <SubmitRecipeClientWrapper />
+      <Suspense fallback={<RecipesSkeleton />}>
+        <SubmitRecipeClientWrapper />
+      </Suspense>
     </main>
   );
 }
